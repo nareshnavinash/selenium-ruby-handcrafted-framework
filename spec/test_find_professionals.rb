9f34de0b27699fd23bcbafe3ada40ee55ec113f2
Naresh Sekar <nareshnavinash@gmail.com>
2020-10-24 14:53:05 +0530

    removing uncommented lines

--- spec/test_find_professionals.rb
+++ spec/test_find_professionals.rb
@@ -24,37 +24,36 @@ describe("Find Professionals in Upwork") do
     @@test_data['search_keyword'].each do |search_key|
         it("Find professionals using search option in Upwork") do
             step("Run " + Config.browser + " brower with headless as " + Config.headless.to_s) do
-                # @@driver = Driver.new
+                @@driver = Driver.new
             end
             step("Clear cookies from the browser") do
-                # @@driver.clear_cookies
+                @@driver.clear_cookies
             end
             step("Navigate to Upwork website - " + Config.url) do
-                # @@driver.get(Config.url)
-                # sleep(60) if @@captcha.captcha.is_present_with_wait?(10)
-                # @@captcha.handle_captcha_page if @@captcha.captcha.is_present_with_wait?(10)
-                # expect_to_equal(@@find_prof_page.find_professionals_and_agencies_text_box.is_present_with_wait?(30), true, "Home page is displayed")
+                @@driver.get(Config.url)
+                sleep(60) if @@captcha.captcha.is_present_with_wait?(10)
+                @@captcha.handle_captcha_page if @@captcha.captcha.is_present_with_wait?(10)
+                expect_to_equal(@@find_prof_page.find_professionals_and_agencies_text_box.is_present_with_wait?(30), true, "Home page is displayed")
             end 
             step("Select Professionals and Agencies in the search dropdown and search for the keyword - " + search_key) do
-                # @@find_prof_page.select_professionals_and_agencies_in_search_dropdown
-                # @@find_prof_page.find_professionals_and_agencies_text_box.flash
-                # @@find_prof_page.find_professionals_and_agencies_text_box.mouse_over
-                # @@find_prof_page.find_professionals_and_agencies_text_box.click
-                # # sleep(60)
-                # @@find_prof_page.search_for_keyword(search_key)
-                # if !@@search_listing.is_search_listing_displayed?
-                #     @@driver.quit
+                @@find_prof_page.select_professionals_and_agencies_in_search_dropdown
+                @@find_prof_page.find_professionals_and_agencies_text_box.flash
+                @@find_prof_page.find_professionals_and_agencies_text_box.mouse_over
+                @@find_prof_page.find_professionals_and_agencies_text_box.click
+                # sleep(60)
+                @@find_prof_page.search_for_keyword(search_key)
+                if !@@search_listing.is_search_listing_displayed?
+                    @@driver.quit
                     @@driver = Driver.new
                     @@driver.clear_cookies
                     @@driver.get(@@test_data["url_with_search_key"] + search_key)
-                # end
+                end
                 expect_to_equal(@@search_listing.is_search_listing_displayed?, true, "Search listing page is displayed")
             end
             step("Parsing the first page with search results and storing the results in hash of hash format") do
                 @@result_hash = @@search_listing.parse_results_page_and_store_values
             end
             step("Validate the results hash along with the search key that is used to get the results") do
-                binding.pry
                 @@result_hash.each do |key, value|
                     pass_result = {}
                     fail_result = {}
